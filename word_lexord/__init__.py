8838ac5abb2982b7de38d838c47b2343cf7bf9e7
lindy2076 <kosolapov.agent@yandex.ru>
2023-04-16 12:45:31 +0300

    minor fixes

--- word_lexord/__init__.py
+++ word_lexord/__init__.py
@@ -5,3 +5,12 @@ from .base import (
     get_word_by_number,
     get_words_numbers_in_sentence
 )
+
+
+__all__ = [
+    "decimal_to_base",
+    "convert_nums_to_letters",
+    "get_word_number",
+    "get_word_by_number",
+    "get_words_numbers_in_sentence"
+]
